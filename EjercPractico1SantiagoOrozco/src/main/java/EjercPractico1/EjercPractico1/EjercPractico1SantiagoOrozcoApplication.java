9b5d58ff78524c912a5f22f21d8c094ca962f860
santiOM0611 <sorozco80826@ufide.ac.cr>
2025-02-24 18:33:42 -0600

    EjercicioPractico1
    Se creó el archivo de Java y su repositorio.

---
+++ EjercPractico1SantiagoOrozco/src/main/java/EjercPractico1/EjercPractico1/EjercPractico1SantiagoOrozcoApplication.java
@@ -0,0 +1,13 @@
+package EjercPractico1.EjercPractico1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercPractico1SantiagoOrozcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercPractico1SantiagoOrozcoApplication.class, args);
+	}
+
+}
---
+++ EjercPractico1SantiagoOrozco/src/test/java/EjercPractico1/EjercPractico1/EjercPractico1SantiagoOrozcoApplicationTests.java
@@ -0,0 +1,13 @@
+package EjercPractico1.EjercPractico1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercPractico1SantiagoOrozcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
